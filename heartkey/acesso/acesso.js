463fcd2bf38edfffe898da3dd02278b023e4c525
Le0odev <leonardovinicius09@hotmail.com>
2024-01-13 02:12:09 -0300

    32423

--- heartkey/acesso/acesso.js
+++ heartkey/acesso/acesso.js
@@ -46,7 +46,7 @@ if(usuario == "rebeca" && senha == "1612"){
     setTimeout(() => {
 
         
-        window.location.replace("/error/error.html");
+        window.location.replace("https://heartkey.vercel.app/error/error.html");
 
         }, 500);
 
