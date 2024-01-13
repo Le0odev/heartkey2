95aeba6e1fb405ff7c4a9cd97fca7e78f09037b0
Le0odev <leonardovinicius09@hotmail.com>
2024-01-13 02:44:31 -0300

    1231

--- acesso2/acesso.js
+++ acesso2/acesso.js
@@ -36,7 +36,7 @@ if(usuario == "rebeca" && senha == "1612"){
     setTimeout(() => {
 
     
-    window.location.replace("https://heartkey.vercel.app/home/index.html");
+    window.location.replace("http://127.0.0.1:5500/acesso2/home.html");
     }, 500);
     
 } else{
@@ -46,7 +46,7 @@ if(usuario == "rebeca" && senha == "1612"){
     setTimeout(() => {
 
         
-        window.location.replace("https://heartkey.vercel.app/error/error.html");
+        window.location.replace("http://127.0.0.1:5500/acesso2/error.html");
 
         }, 500);
 
